e32ff34e29e2ade7d5468897a606a107234a230d
Mariusz <Mariusz@localhost>
2010-08-05 23:41:35 +0200

    Network in facade

--- NeuralBasic/Network.h
+++ NeuralBasic/Network.h
@@ -1,10 +1,22 @@
 #pragma once
+// The following ifdef block is the standard way of creating macros which make exporting 
+// from a DLL simpler. All files within this DLL are compiled with the NEURALBASIC_EXPORTS
+// symbol defined on the command line. This symbol should not be defined on any project
+// that uses this DLL. This way any other project whose source files include this file see 
+// NEURALBASIC_API functions as being imported from a DLL, whereas this DLL sees symbols
+// defined with this macro as being exported.
+#ifdef NEURALBASIC_EXPORTS
+#define NEURALBASIC_API __declspec(dllexport)
+#else
+#define NEURALBASIC_API __declspec(dllimport)
+#endif
 
 #include "Layer.h"
 
+
 namespace NeuralBasic
 {
-	class Network
+	class NEURALBASIC_API Network
 	{
 	public:
 		explicit Network(const std::vector<size_t>& networkSize);
@@ -16,8 +28,11 @@ namespace NeuralBasic
 	private:
 		size_t GetInputCount() const;
 
+#pragma warning( push )
+#pragma warning( disable : 4251 )
 		typedef std::vector<Layer> Layers;
 		Layers m_layers;
+#pragma warning( pop )
 	};
 
 }
